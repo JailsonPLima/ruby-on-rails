096f08a767891c1ae9274f0b64c3e133122f0800
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2025-08-04 13:39:00 -0300

    feat: add about page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#home"
+
+  # Defines the path of sub-routes
+  get "about", to: "pages#about"
 end
